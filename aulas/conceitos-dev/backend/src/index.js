e0981a5334c66f5169ee715a7fb532226c0d06b4
pr-contra <pedro.rcontra@gmail.com>
2021-02-01 01:00:28 +0000

    Lecture 'Configurando Nodemon'
    - Added Nodemon library for auto-refresh and added script to package.json
    
    - Added dotenv library to be able to read configuration variables from .env file

--- aulas/conceitos-dev/backend/src/index.js
+++ aulas/conceitos-dev/backend/src/index.js
@@ -1,5 +1,7 @@
 const { response } = require("express");
 const express = require("express");
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
 
@@ -9,4 +11,4 @@ app.get("/projects", (_, response) => {
 });
 
 // O Port tem de ser acima da 80
-app.listen(3333);
+app.listen(process.env.PORT, () => { console.log(`Listening to port ${process.env.PORT}`) });
