ec50d69b9c6e5d1fe718a080cdcfa80bae1aafc6
pr-contra <pedro.rcontra@gmail.com>
2021-02-01 19:04:59 +0000

    Lecture 'Tipos de parametros'
    Added a raw version of a basic post/put/delete request

--- aulas/conceitos-dev/backend/src/index.js
+++ aulas/conceitos-dev/backend/src/index.js
@@ -1,16 +1,50 @@
-const { response } = require("express");
 const express = require("express");
+
 const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
 
-app.get("/projects", (_, response) => {
+app.use(express.json());
+
+/**
+ * Metodos HTTP:
+ *
+ * GET: Get data from backend
+ * POST: Create info into backend
+ * PUT/PATCH: Update info in backend
+ * DELETE: Delete info in backend
+ */
+
+ /**
+  * Types of params:
+  *
+  * Query params: Filters and pagination
+  * Route params: Identify resources (Update/Delete)
+  * Request body: Content to be Created/Updated
+  */
+
+app.get("/projects", (request, response) => {
+  const params = request.params;
+  const {title, owner} = request.query;
+  const body = request.body;
+
+  console.log({title, owner});
   // return response.send("Hello world!");
   return response.json({message: "Hello world!"});
 });
 
 app.post("/projects", (request, response) => {
+  // return response.status(201).send("Created!");
+  return response.json(["project1","project2"]);
+});
+
+app.put("/projects/:id", (request, response) => {
+  // return response.status(201).send("Created!");
+  return response.json(["project1","project2"]);
+});
+
+app.delete("/projects/:id", (request, response) => {
   // return response.status(201).send("Created!");
   return response.json(["project1","project2"]);
 });
