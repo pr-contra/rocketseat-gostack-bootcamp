b31860a52ad64deb25adf4a2e0097e446becdb67
pr-contra <pedro.rcontra@gmail.com>
2021-02-01 19:59:41 +0000

    Lecture 'Aplicação Funcional'
    Added basic POST/PUT/DELETE requests which modifies the contents of a local array list

--- aulas/conceitos-dev/backend/src/index.js
+++ aulas/conceitos-dev/backend/src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const dotenv = require('dotenv');
 dotenv.config();
@@ -16,37 +17,65 @@ app.use(express.json());
  * DELETE: Delete info in backend
  */
 
- /**
-  * Types of params:
-  *
-  * Query params: Filters and pagination
-  * Route params: Identify resources (Update/Delete)
-  * Request body: Content to be Created/Updated
-  */
+/**
+* Types of params:
+*
+* Query params: Filters and pagination
+* Route params: Identify resources (Update/Delete)
+* Request body: Content to be Created/Updated
+*/
+
+const projects = [];
 
 app.get("/projects", (request, response) => {
-  const params = request.params;
-  const {title, owner} = request.query;
-  const body = request.body;
+  const { title } = request.query;
 
-  console.log({title, owner});
-  // return response.send("Hello world!");
-  return response.json({message: "Hello world!"});
+  console.log(title);
+
+  const result = title ?
+    projects.filter(p => p.title.includes(title)) : projects;
+
+  return response.json(result);
 });
 
 app.post("/projects", (request, response) => {
-  // return response.status(201).send("Created!");
-  return response.json(["project1","project2"]);
+  const {title, owner} = request.body;
+
+  const project = { id: uuidv4(), title, owner };
+  projects.push(project);
+
+  return response.status(201).send();
 });
 
 app.put("/projects/:id", (request, response) => {
-  // return response.status(201).send("Created!");
-  return response.json(["project1","project2"]);
+  const {id} = request.params;
+  const {title, owner} = request.body;
+
+  const projectIndex = projects.findIndex(p => p.id === id);
+
+  if(projectIndex < 0){
+    return response.status(400).json( {error: "Project not found."} );
+  }
+
+  // projects[projectIndex] = {id: id, title: title, owner: owner};
+  // OR
+  projects.splice(projectIndex,1,{id: id, title: title, owner: owner});
+
+  return response.json(projects);
 });
 
 app.delete("/projects/:id", (request, response) => {
-  // return response.status(201).send("Created!");
-  return response.json(["project1","project2"]);
+  const {id} = request.params;
+
+  const projectIndex = projects.findIndex(p => p.id === id);
+
+  if(projectIndex < 0){
+    return response.status(400).json( {error: "Project not found."} );
+  }
+
+  projects.splice(projectIndex,1);
+
+  return response.send(204).send();
 });
 
 // O Port tem de ser acima da 80
